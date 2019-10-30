aefc8c0b3ac95a263c51de2d455aff895dae621a
xearts <xearts@mbp-13-early2011-01.local>
2019-10-30 18:50:24 +0900

    pagerを実装

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,6 +11,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Knp\Component\Pager\PaginatorInterface;
+
 
 class ArticleController extends AbstractController
 {
@@ -20,10 +22,18 @@ class ArticleController extends AbstractController
      * @param ArticleRepository $articleRepository
      * @Route("/", name="article_index", methods={"GET"})
      */
-    public function index(ArticleRepository $articleRepository): Response
+    public function index(ArticleRepository $articleRepository, Request $request, PaginatorInterface $paginator): Response
     {
+        $articles = $articleRepository->findAll();
+
+        $article = $paginator->paginate(
+            $articles,
+            $request->query->getInt('page', 1),
+            3
+        );
+
         return $this->render('article/index.html.twig', [
-            'articles' => $articleRepository->findAll(),
+            'articles' => $article,
         ]);
     }
 
